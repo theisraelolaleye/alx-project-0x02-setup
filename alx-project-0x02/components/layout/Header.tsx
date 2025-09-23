d1fcc6da48ffb812b43e0bbe4c53ff348f6d9d17
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-23 02:42:59 +0100

    feat: add header navigation and home/about pages

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
   )
 }
 
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <section>
+      <h1>AboutPage</h1>
+    </section>
+  )
+}
+
+export default AboutPage
