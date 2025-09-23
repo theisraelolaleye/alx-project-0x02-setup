1392ad5028481434fc4d248140710f772b3796eb
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-23 04:04:29 +0100

    feat: add customizable Button component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Button from '@/components/common/Button'
 
 const AboutPage = () => {
   return (
     <section>
       <h1>AboutPage</h1>
+      <Button size="large" shape="rounded-md">Click Me</Button>
+      <Button size="small" shape="rounded-sm">Click Me</Button>
+      <Button size="medium" shape="rounded-full">Click Me</Button>
     </section>
   )
 }
